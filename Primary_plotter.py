892d5ff73270ef4d0c1d049f622cb06c0ddfa1d5
BezuidenhoutMC <bezmc93@gmail.com>
2020-03-05 16:29:28 +0000

    Update 05/03/2020
    detections_plot:
    resolved a type issue in np.genfromtxt
    added capability of marking known coordinates on beam map
    added compatibility with config file that Primary_plotter.py uses (i.e. beam parameters in config file instead of as cmd line inputs)
    
    Primary_plotter.py:
    corrected confusion with primary beam heights/widths (now assuming circular)
    corrected MeerKAT latitude

--- Primary_plotter.py
+++ Primary_plotter.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Code to plot the MeerKAT primary beam and TABs for a given pointing
+configuration.
+'''
 
 #Size of primary beam in deg at various frequencies (uncorrected):
 freqs = [856,963,1070,1177,1284,1391,1498,1605] 
 h = [1.70,1.56,1.37,1.23,1.11,1.02,0.96,0.90]
-v = [1.68,1.48,1.33,1.17,1.05,0.98,0.94,0.90]
+'''NOTE: I assume a circular primary beam with diameter equal to
+   the size in the horizontal polarisation direction. There'll be
+   a small error due to this.
+'''
+# v = [1.68,1.48,1.33,1.17,1.05,0.98,0.94,0.90]
 
 
 import numpy as np
@@ -14,7 +21,7 @@ from matplotlib import pyplot as plt
 import math
 import configparser
 
-latitude = np.deg2rad(-33)
+latitude = np.deg2rad(-30.721)
 
 
 parser = argparse.ArgumentParser()
@@ -30,13 +37,14 @@ el_heights = config['DEFAULT']['EL_HEIGHTS'].split(',')
 el_widths = config['DEFAULT']['EL_WIDTHS'].split(',')
 el_angles = config['DEFAULT']['EL_ANGLES'].split(',')
 
+
 if len(decs) == 1:
 	axes = [0]
 	f,axis = plt.subplots(len(decs),1,sharex=False,facecolor='w')	
 	axes[0] = axis
 else:
 	f,axes = plt.subplots(len(decs),1,sharex=False,facecolor='w')
-	
+
 for i in range (0,len(decs)):
 	print('----'+str(i)+'--------------------------')
 
@@ -56,8 +64,8 @@ for i in range (0,len(decs)):
 	print("Height at "+str(decs[i])+"deg Dec:  " + str(tile_height) + " deg")
 	print("Width at "+str(decs[i])+"deg Dec:  " + str(tile_width) + " deg")
 	print("Area at "+str(decs[i])+"deg Dec:  " + str(tile_area) + " deg^2")
-
 	print("")
+	
 	axes[i].annotate(str(round(tile_area,3))+'\ndeg^2',xy=(np.median(pulse["RA"]),np.median(pulse["Dec"])),bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=0.3))
 
 
@@ -77,6 +85,8 @@ for i in range (0,len(decs)):
 	sin_boresight_alt = (np.sin(np.deg2rad(decs[i]))*np.sin(latitude))+(np.cos(np.deg2rad(decs[i]))*np.cos(latitude)*np.cos(has[i]))
 	boresight_alt = np.arcsin(sin_boresight_alt) 
 
+	# if decs[i] > 0:
+	#	 boresight_alt = 2*math.pi - boresight_alt
 
 	cos_boresight_azim = (np.sin(np.deg2rad(decs[i])) - np.sin(boresight_alt)*np.sin(latitude)) / (np.cos(boresight_alt) *np.cos(latitude))
 	if cos_boresight_azim > 1:
@@ -109,7 +119,7 @@ for i in range (0,len(decs)):
 		RA1 = np.arcsin(sinRA1)
 		WIDTH = np.rad2deg(RA1-RA0)
 
-		HEIGHT = np.rad2deg(np.deg2rad(v[q]))
+		HEIGHT = h[q]
 
 		e=ptch.Ellipse(xy=(pulse["RA"][0],pulse["Dec"][0]),
 		width=WIDTH,height=HEIGHT,angle=0,fill=False,color=color[q])
--- detections_plot.py
+++ detections_plot.py
@@ -1,46 +1,69 @@
 #!/usr/bin/env python
+'''
 
+'''
 import numpy as np
 import argparse
+import configparser
 import matplotlib.patches as ptch
 from matplotlib import pyplot as plt
 from astropy.coordinates import SkyCoord
 import astropy.units  as u
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', dest='file', nargs = 1, type = str, help="Beam coordinates file",required=True)
-parser.add_argument('--w',dest = 'el_width',nargs = 1, type = float, required = True)
-parser.add_argument('--h',dest = 'el_height',nargs = 1, type = float, required = True)
-parser.add_argument('--a',dest = 'el_angle',nargs = 1, type = float, required = True)
-parser.add_argument('--d',dest = 'dets',nargs = '+', type = int, required = False)
+parser.add_argument('-c', dest='coords', nargs = 1, type = str, help="Beam coordinates file",
+					required=True)
+parser.add_argument('-f', dest='file', nargs = 1, type = str, help="Candidate file")
+parser.add_argument('--w',dest = 'el_width',nargs = 1, type = float)
+parser.add_argument('--h',dest = 'el_height',nargs = 1, type = float)
+parser.add_argument('--a',dest = 'el_angle',nargs = 1, type = float)
+parser.add_argument('--d',dest = 'dets',nargs = '+', type = int)
+parser.add_argument('--k',dest = 'known', nargs = 2, type=float, metavar=('RA', 'DEC'),
+					action='append',help='source position in RADEC or AziAlt')
+
 options= parser.parse_args()
 
-options.el_width[0]/=3600
-options.el_height[0]/=3600
+if options.file is not None:
+	config = configparser.ConfigParser()
+	config.read(options.file)
+	el_height = float(config['DEFAULT']['EL_HEIGHTS'].split(',')[0])
+	el_width = float(config['DEFAULT']['EL_WIDTHS'].split(',')[0])
+	el_angle = float(config['DEFAULT']['EL_ANGLES'].split(',')[0])
 
-pulse = np.genfromtxt(options.file[0],delimiter=' ',dtype=None,names=["RA","Dec"],encoding="ascii")
+else:
+	if options.el_height == None or options.el_width == None or options.el_angle == None:
+		print('Error: If you are not using a config file, you have to specify the the beam height, width, and '
+				'orientation angle with command line options.')
+		exit()
+	el_height = options.el_height[0]
+	el_width = options.el_width[0]
+	el_angle = options.el_angle[0]
 
-ax = plt.gca()
-sc=plt.scatter(pulse["RA"],pulse["Dec"],s=3)
+el_width/=3600
+el_height/=3600
+
+pulse = np.genfromtxt(options.coords[0],delimiter=' ',dtype=float,names=["RA","Dec"],encoding="ascii")
+
+fig,ax = plt.subplots(1,1)
+ax.scatter(pulse["RA"],pulse["Dec"],s=3)
+
+if options.known is not None:
+	for sourceCoord in options.known:
+		ax.scatter(sourceCoord[0],sourceCoord[1],marker='x',color='red')
+
 for i in range(0,len(pulse["RA"])):
 	if i in options.dets:
-		e=ptch.Ellipse(xy=(pulse["RA"][i],pulse["Dec"][i]),width=2*options.el_width[0],height=2*options.el_height[0],angle=options.el_angle[0],fill=True,color='magenta')
+		e=ptch.Ellipse(xy=(pulse["RA"][i],pulse["Dec"][i]),width=2*el_width,height=2*el_height,
+					   angle=el_angle,fill=True,color='magenta')
 		ax.add_artist(e)
-	e=ptch.Ellipse(xy=(pulse["RA"][i],pulse["Dec"][i]),width=2*options.el_width[0],height=2*options.el_height[0],angle=options.el_angle[0],fill=False,color='green')
+	
+	e=ptch.Ellipse(xy=(pulse["RA"][i],pulse["Dec"][i]),width=2*el_width,height=2*el_height,
+				   angle=el_angle,fill=False,color='green')
 	ax.add_artist(e)
 	
 plt.xlabel('RA (deg)')
 plt.ylabel('Dec (deg)')
 
-#RAs = ["16:00:54.84", "16:00:40.08","16:01:09.59","16:00:45.25","16:01:00.00"]
-#Decs = ["-50:44:22.7", '-50:45:19.5', '-50:43:25.7','-50:43:59.3','-50:43:02.4']
-#RAs = ["19:35:31.26", "19:35:19.87","19:32:55.45", "19:33:18.33"]
-#Decs = ["-62:07:49.2", '-62:07:13.0', '-62:21:03.6',"-62:22:17.4"]
-
-
-#for i in range(0,len(RAs)):
-#	co = SkyCoord(RAs[i],Decs[i], frame="icrs",unit=(u.hourangle,u.deg))
-#	plt.scatter(co.ra.deg,co.dec.deg,marker='x',color='red',zorder=7)	
 plt.show()
 
 
